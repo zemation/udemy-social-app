e90dd4bff57d7bef32a6e7f98b20c2ad4845a889
Robert Rodgers <rbrandon@zemation.com>
2019-09-20 12:43:25 -0600

    All lessons up to live search

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
+const markdown = require('marked')
+const sanitizeHTML = require('sanitize-html')
 const app = express()
 
 let sessionOptions = session({
@@ -15,13 +17,33 @@ let sessionOptions = session({
 app.use(sessionOptions)
 app.use(flash())
 
+app.use(function(req, res, next){
+    // make our markdown function available from within ejs templates
+    res.locals.filterUserHTML = function(content) {
+        return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAtributes: {}})
+    }
+
+
+    // make all error and success flash messages available from all templates
+    res.locals.errors = req.flash("errors")
+    res.locals.success = req.flash("success")
+
+
+    // make current user id available on the req object
+    if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+
+
+    // make user session data available from within view templates 
+    res.locals.user = req.session.user
+    next()
+})
+
 const router = require('./router')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.use(express.static('public'))
-
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
