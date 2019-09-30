31ed57f703c01332382e90b8e2dc21475f99190b
Robert Rodgers <rbrandon@zemation.com>
2019-09-30 11:11:50 -0600

    Added Send Grid support to create post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,6 @@
 const Post = require('../models/Post')
+const sendgrid = require('@sendgrid/mail')
+sendgrid.setApiKey(process.env.SENDGRIDAPIKEY)
 
 exports.viewCreateScreen = function(req, res) {
   res.render('create-post')
@@ -7,6 +9,13 @@ exports.viewCreateScreen = function(req, res) {
 exports.create = function(req, res) {
   let post = new Post(req.body, req.session.user._id)
   post.create().then(function(newId) {
+    sendgrid.send({
+      to: 'rbrandon@zemation.com',
+      from: 'zemation@zemation.com',
+      subject: 'Thank you for Posting',
+      text: 'Your post was submitted successfully',
+      html: 'Your post was submitted <strong>successfully</strong>'
+    })
     req.flash("success", "New post successfully created.")
     req.session.save(() => res.redirect(`/post/${newId}`))
   }).catch(function(errors) {
